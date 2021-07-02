c37b87cdaea6e03610612e2234a29956fdc4fe00
neo <neo@academ.org>
2021-07-03 03:24:09 +0700

    try to fix logs attachment

--- src/test/java/ru/detmir/job/helpers/AllureAttachments.java
+++ src/test/java/ru/detmir/job/helpers/AllureAttachments.java
@@ -1,25 +1,18 @@
 package ru.detmir.job.helpers;
 
 import com.codeborne.selenide.Selenide;
-import io.qameta.allure.Allure;
 import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
-import static com.codeborne.selenide.Selenide.sleep;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static org.openqa.selenium.logging.LogType.BROWSER;
 import static ru.detmir.job.helpers.DriverUtils.getVideoUrl;
 
 public class AllureAttachments {
-    //@Attachment(value = "{attachName} Text", type = "text/plain")
+    @Attachment(value = "{attachName} Text", type = "text/plain")
     public static String attachAsText(String attachName, String message) {
         return message;
     }
@@ -29,12 +22,9 @@ public class AllureAttachments {
         return ((TakesScreenshot) getWebDriver()).getScreenshotAs(OutputType.BYTES);
     }
 
-    @Attachment(value = "{attachName} Log", type = "text/plain")
+    @Attachment(value = "Browser console logs", type = "text/plain")
     public static void addBrowserConsoleLogs() {
-        attachAsText(
-                "Browser console logs",
-                String.join("\n", Selenide.getWebDriverLogs(BROWSER))
-        );
+        String.join("\n", Selenide.getWebDriverLogs(BROWSER));
     }
 
     @Attachment(value = "Page source", type = "text/plain")
@@ -48,25 +38,4 @@ public class AllureAttachments {
                 + getVideoUrl(sessionId)
                 + "' type='video/mp4'></video></body></html>";
     }
-
-/*    @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
-    public static void addVideo(String sessionId) {
-        URL videoUrl = getVideoUrl(sessionId);
-        if (videoUrl != null) {
-            InputStream videoInputStream = null;
-            sleep(1000);
-
-            for (int i = 0; i < 10; i++) {
-                try {
-                    videoInputStream = videoUrl.openStream();
-                    break;
-                } catch (FileNotFoundException e) {
-                    sleep(1000);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            Allure.addAttachment("Video", "video/mp4", videoInputStream, "mp4");
-        }
-    }*/
 }
