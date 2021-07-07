59e8fa204aa6f44dabe215c49778fe78633dbbd2
neo <neo@academ.org>
2021-07-08 02:28:18 +0700

    delete brackets {} from steps; add config files from gitignore

--- src/test/java/ru/detmir/job/tests/ContentVacanciesPageTests.java
+++ src/test/java/ru/detmir/job/tests/ContentVacanciesPageTests.java
@@ -21,21 +21,17 @@ public class ContentVacanciesPageTests extends BaseTest {
     @Description("Show more of vacancies test")
     @DisplayName("Тест на проверку кнопки показа всех вакансий")
     void checkMoreVacanciesButton() {
-        step("Open https://job.detmir.ru/", () -> {
-            Selenide.open("https://job.detmir.ru/");
-        });
+        step("Open https://job.detmir.ru/", () ->
+                Selenide.open("https://job.detmir.ru/"));
 
-        step("Перейти через левое меню в вакансии", () -> {
-            $$("ul li a").findBy(text("Вакансии")).click();
-        });
+        step("Перейти через левое меню в вакансии", () ->
+                $$("ul li a").findBy(text("Вакансии")).click());
 
-        step("Выбрать категорию вакансий 'Информационные технологии'", () -> {
-            $$(".vacancies-link").findBy(text("Информационные технологии")).click();
-        });
+        step("Выбрать категорию вакансий 'Информационные технологии'", () ->
+                $$(".vacancies-link").findBy(text("Информационные технологии")).click());
 
-        step("Больше вакансий - нажать на кнопку 'показать еще'", () -> {
-            $("div .vacancies-more__show-more").scrollIntoView(true).click();
-        });
+        step("Больше вакансий - нажать на кнопку 'показать еще'", () ->
+                $("div .vacancies-more__show-more").scrollIntoView(true).click());
 
         step("Проверить, что доступно более 11 вакансий на странице", () -> {
             String consoleLogs = DriverUtils.getConsoleLogs();
@@ -50,9 +46,8 @@ public class ContentVacanciesPageTests extends BaseTest {
     @Description("Check vacancies page metro station filter")
     @DisplayName("Тест фильтра по станции метро")
     void checkVacanciesPageMetroStationFilter() {
-        step("Открыть страницу вакансий на сайте https://job.detmir.ru", () -> {
-            Selenide.open("https://job.detmir.ru/vacancies/");
-        });
+        step("Открыть страницу вакансий на сайте https://job.detmir.ru", () ->
+                Selenide.open("https://job.detmir.ru/vacancies/"));
 
         step("Выбрать метро - Окружная (МЦК)", () -> {
             $("#subway").$(".subway__input").val("Окружная");
@@ -74,28 +69,22 @@ public class ContentVacanciesPageTests extends BaseTest {
     @Description("Check automation QA vacancy")
     @DisplayName("Тест вакансии ведущего тестировщика по автоматизации")
     void checkAutomationQAVacancy() {
-        step("Открыть страницу вакансий на сайте https://job.detmir.ru", () -> {
-            Selenide.open("https://job.detmir.ru/vacancies/");
-        });
+        step("Открыть страницу вакансий на сайте https://job.detmir.ru", () ->
+                Selenide.open("https://job.detmir.ru/vacancies/"));
 
-        step("Выбрать категорию ИТ", () -> {
-            $$(".vacancies-link").findBy(text("Информационные технологии")).click();
-        });
+        step("Выбрать категорию ИТ", () ->
+                $$(".vacancies-link").findBy(text("Информационные технологии")).click());
 
-        step("Выбрать 3 страницу вакансий", () -> {
-            $(".vacancies-more-pagination__link", 2).click();
-        });
+        step("Выбрать 3 страницу вакансий", () ->
+                $(".vacancies-more-pagination__link", 2).click());
 
-        step("Найти вакансию ведущего тестировщика по автоматизации", () -> {
-            $$(".vacancies-more-item__name").findBy(text("Automation QA")).click();
-        });
+        step("Найти вакансию ведущего тестировщика по автоматизации", () ->
+                $$(".vacancies-more-item__name").findBy(text("Automation QA")).click());
 
-        step("Проверить, что в требованиях есть Selenide", () -> {
-            $(withText("Наш стек: Java 16, Selenide, Rest Assured")).shouldBe(visible);
-        });
+        step("Проверить, что в требованиях есть Selenide", () ->
+                $(withText("Наш стек: Java 16, Selenide, Rest Assured")).shouldBe(visible));
 
-        step("Проверить, что зп = 350т", () -> {
-            $(".vacancy-detail__price").shouldHave(text("до 350000 ₽ до вычета налогов"));
-        });
+        step("Проверить, что зп = 350т", () ->
+                $(".vacancy-detail__price").shouldHave(text("до 350000 ₽ до вычета налогов")));
     }
 }
